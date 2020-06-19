56f6c18ac0019802c5dc92cd8b6c0f5abbc309a5
vadim starodubov <startradc@gmail.com>
2020-06-19 21:51:39 +0300

    Created template script with Settings Handler

---
+++ IngameScript1/HandSettings.cs
@@ -0,0 +1,58 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public class HandSettings : ISettings
+        {
+            static MyIni _ini = new MyIni();
+            public HandSettings()
+            {
+            }
+
+            public string PistonSufix;
+            public decimal PistonMaxSpeed;
+
+/*
+[HandSettings]
+PistonSufix=hnd
+PistonMaxSpeed=0.5
+*/
+            public void ParseString(string store)
+            {
+                MyIniParseResult result;
+                if (!_ini.TryParse(store, out result))
+                    throw new Exception(result.ToString());
+
+                PistonSufix = _ini.Get(nameof(HandSettings), nameof(PistonSufix)).ToString();
+                PistonMaxSpeed = _ini.Get(nameof(HandSettings), nameof(PistonMaxSpeed)).ToDecimal();
+            }
+
+            public string ComposeString()
+            {
+                _ini.Clear();
+                _ini.Set(nameof(HandSettings), nameof(PistonSufix), PistonSufix);
+                _ini.Set(nameof(HandSettings), nameof(PistonMaxSpeed), PistonMaxSpeed);
+                return _ini.ToString();
+            }
+        }
+    }
+}
---
+++ SETools/SettingsTools.cs
@@ -0,0 +1,60 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public interface ISettings{
+            void ParseString(string store);
+            string ComposeString();
+        }
+
+        public class SettingsHandler<T> where T : ISettings, new()
+        {
+            Program _program;
+            public SettingsHandler(Program program)
+            {
+                _program = program;
+            }
+
+            public T GetSettings()
+            {
+                string stringSettings = string.IsNullOrWhiteSpace(_program.Storage) 
+                    ? _program.Me.CustomData
+                    : _program.Storage;
+                
+                _program.Echo(stringSettings);
+
+                return ParseToObject(stringSettings);
+            }
+            public T ResetSettings()
+            {
+                string stringSettings = _program.Me.CustomData;
+                _program.Echo(stringSettings);
+                return ParseToObject(stringSettings);
+            }
+
+            private static T ParseToObject(string stringSettings)
+            {
+                var settings = new T();
+                settings.ParseString(stringSettings);
+                return settings;
+            }
+        }
+    }
+}
