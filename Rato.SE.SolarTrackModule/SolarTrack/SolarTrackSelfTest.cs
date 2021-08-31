71e76ffb51390ef38bfc3ee77f888c8f4c11d59f
Vadim Starodubov <vadim.starodubov@railsreactor.com>
2021-08-31 16:37:24 +0300

    Move Solar track to it's own shared project

---
+++ Rato.SE.SolarTrackModule/SolarTrack/SolarTrackSelfTest.cs
@@ -0,0 +1,64 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public partial class SolarTrackModule : IControllModule
+        {
+            public UpdateFrequency StartTestSquence()
+            {
+                return _trackController.StartSequence(SelfTestSequence());
+            }
+
+            private IEnumerator<int> MakeFullRound(IMyMotorStator rotor) {
+                var startAngle = rotor.Angle;
+                SetrotorVelocity(rotor, MathHelper.Pi / _settings.TestSpeedDividor);
+                yield return 5;
+                while (Math.Abs(_ratoMath.ShortestAnglePath(rotor.Angle, startAngle)) > 0.02) {
+                    yield return 1;
+                }
+                SetrotorVelocity(rotor, 0);
+            }
+
+            private IEnumerator<int> SelfTestSequence()
+            {
+                Status = ModuleStatus.SelfTest;
+
+                var round1 = MakeFullRound(_rotor1);
+                while (round1.MoveNext()) yield return round1.Current;
+
+                if (_rotor2 != null)
+                {
+                    var round2 = MakeFullRound(_rotor2);
+                    while (round2.MoveNext()) yield return round2.Current;
+                }
+
+                StatusDetails.Add(new ModuleStatusDetail()
+                {
+                    Name = "Self test",
+                    Level = ActionStatus.Ok
+                });
+
+                Status = ModuleStatus.ReadyToStart;
+            }
+        }
+    }
+}
